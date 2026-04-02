47cf4bd1dfb7ea278b03bc41658c8461cd7f05a0
Shantal <shantalcastilloencarnacion@gmail.com>
2026-04-02 02:37:34 -0400

    feat: agregar ConexionDB y estructura de carpetas dao/util

---
+++ gym-system/src/com/gym/util/ConexionDB.java
@@ -0,0 +1,22 @@
+package com.gym.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConexionDB {
+
+    private static final String URL = "jdbc:mysql://localhost:3306/gimnasio_db";
+    private static final String USUARIO = "root";
+    private static final String PASSWORD = "Sc.20062019"; 
+
+    public static Connection getConexion() {
+        try {
+            Connection conexion = DriverManager.getConnection(URL, USUARIO, PASSWORD);
+            return conexion;
+        } catch (SQLException e) {
+            System.out.println("Error al conectar: " + e.getMessage());
+            return null;
+        }
+    }
+}
